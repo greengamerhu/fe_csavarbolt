166490caa315de0953d56e5e12e2fa2fb58ff83f
Rimóczi Dániel <rimoczidani007@gmail.com>
2023-01-31 15:40:12 +0100

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { ftruncateSync } from 'fs';
-import Popup from 'reactjs-popup';
+import { throws } from 'assert';
 
 interface State {
   postTipus : string,
@@ -11,6 +11,11 @@ interface State {
   postHossz: number,
   postAr: number,
   csavarok : Csavar[]
+  show : boolean;
+  selectedItem : Csavar
+  rendelesDarab: number;
+  rendelesDarabNax : number
+  rendelesDarabNin : number
 }
 
 interface Csavar {
@@ -34,9 +39,17 @@ class App extends React.Component<{}, State> {
       postKeszlet : 0,
       postHossz: 0,
       postAr: 0,
-      csavarok : []
+      csavarok : [],
+      show : false,
+      rendelesDarab: 0,
+      rendelesDarabNin :0,
+      rendelesDarabNax : 0,
+
+      selectedItem : {id : -1, tipus: "", hossz : 0, keszlet : 0, ar : 0}
     }
   }
+  toggle = () => this.setState((currentState) => ({show: !currentState.show}));
+  // toggle = () => this.setState({show : true});
   addToDB = async () => {
     const {postTipus, postKeszlet, postHossz, postAr} = this.state
 
@@ -75,16 +88,47 @@ class App extends React.Component<{}, State> {
     })
     this.loadData()
   }
-
-
-  popUprendeles = async (id : number) => {
+  handleClick =  (item : Csavar) => {
     
+    this.setState({
+      selectedItem : item,
+      show: true
+      
+      
+     })
   }
+
+  resetDisplay = () => {
+    this.setState({
+      rendelesDarab : 0,
+      show : false,
+      selectedItem : {id : -1, tipus: "", hossz : 0, keszlet : 0, ar : 0}
+      
+    })
+
+  }
+  megrendel = async (id :number) => {
+    let rendeltdarabszam = { 
+      "db" : this.state.rendelesDarab 
+    }
+    let url = "http://localhost:3000/csavar/"+ id+"/rendeles"
+    let response = await fetch(url, {
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json"
+      } ,
+      body : JSON.stringify(rendeltdarabszam)
+      
+    })
+    this.resetDisplay()
+    this.loadData()
+  }
+
+
    render() {
     const {postTipus, postKeszlet, postHossz, postAr} = this.state
-    const PopupExample = () => (  <Popup trigger={<button> Trigger</button>} position="right center">   
-     <div>Popup content here !!</div>  </Popup>);
     return <div>
+   
       <div className='container'>
           <div className='row justify-content-center' >
             <div className="col-sm-4 text-center">
@@ -112,7 +156,6 @@ class App extends React.Component<{}, State> {
           <div className='row justify-content-center mt-3  ' >
             <div className="col text-center "> 
             <button className='btn btn-secondary ' onClick={this.addToDB}>Felvesz</button> <br />
-            <button className='btn btn-secondary ' onClick={PopupExample}>Rendelés</button> <br />
           
             </div>
           </div>
@@ -123,11 +166,31 @@ class App extends React.Component<{}, State> {
           </div>
          
         </div>
+
+
+
+        {this.state.show && <div className='row justify-content-center pt-3 pb-3 '>
+        <div className="col text-center "> 
+          <h2>Biztos megrendeled ezt a csavart? <strong>{this.state.selectedItem.tipus}</strong></h2>
+            <input type="number" name="db" value={this.state.rendelesDarab} onChange={e => this.setState({rendelesDarab : parseInt(e.currentTarget.value)})}/> db <br />
+            <button onClick={() => this.megrendel(this.state.selectedItem.id) } className='btn btn-success mt-2'>Megrendel</button>
+            <br />
+            <button onClick={this.toggle} className='btn btn-danger mt-2'>Mégse</button>
+            
+            </div>
+         
+          </div>}
+
+
+
+
+
         <div className='row'>
           {this.state.csavarok.map(item => (
+            
             <div className='col-md-4'>
               <div className="card text-center">
-                <div className="card-body">
+                <div className="card-body" >
                   Tipus: {item.tipus} <br />
                   Hossz: {item.hossz} mm <br />
                   Készlet : {item.keszlet} db <br />
@@ -135,14 +198,15 @@ class App extends React.Component<{}, State> {
                   </div>
                 <div className='card-footer'>
                     <button onClick={(event) => this.deleteFromDatabase(item.id)}>Törlés</button>
+                    <button onClick={(event) => this.handleClick(item)}>Rendel</button>
                 </div>
             </div>
           </div>
           ))}
           </div>
           
-      
-    
+        
+          
       
       </div>
 
